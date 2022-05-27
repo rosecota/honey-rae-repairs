110d22810ed20e4790795e5cebf9821bf8a149b4
Rose Cota <rosejcota@gmail.com>
2022-05-27 12:20:33 -0500

    fetch initial ticket list state

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -5,9 +5,28 @@ export const TicketList = () => {
 
   useEffect(
     () => {
-      console.log("Initial state of tickets", tickets); // View the initial state of tickets
+      fetch(`http://localhost:8088/serviceTickets`)
+        .then((response) => response.json())
+        .then((ticketArray) => {
+          setTickets(ticketArray);
+        });
+      //   console.log("Initial state of tickets", tickets); // View the initial state of tickets
     },
     [] // When this array is empty, you are observing initial component state
   );
-  return <h2>List of Tickets</h2>;
+  return (
+    <>
+      <h2>List of Tickets</h2>
+      <article className="tickets">
+        {tickets.map((ticket) => {
+          return (
+            <section>
+              <header>{ticket.description}</header>
+              <footer>Emergency: {ticket.emergency ? "🧨" : "No"}</footer>
+            </section>
+          );
+        })}
+      </article>
+    </>
+  );
 };
