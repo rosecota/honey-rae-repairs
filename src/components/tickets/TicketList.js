982a375bc786caa845e71f2d4e8e4f842e0bf01b
Rose Cota <rosejcota@gmail.com>
2022-05-31 16:09:12 -0500

    add unique key for ticket list

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -36,7 +36,7 @@ export const TicketList = () => {
       <article className="tickets">
         {filteredTickets.map((ticket) => {
           return (
-            <section className="ticket">
+            <section className="ticket" key={`ticket-$ticket.id}`}>
               <header>{ticket.description}</header>
               <footer>Emergency: {ticket.emergency ? "🧨" : "No"}</footer>
             </section>
