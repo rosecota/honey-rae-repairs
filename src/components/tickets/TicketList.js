e650ffa3badaec80abe507f1971394f6955fc039
Rose Cota <rosejcota@gmail.com>
2022-06-06 13:44:56 -0500

    Add functionality for user to create new ticket from ui

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,26 +1,31 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./Tickets.scss";
 
 export const TicketList = () => {
   const [tickets, setTickets] = useState([]);
   const [filteredTickets, setFiltered] = useState([]);
   const [emergency, setEmergency] = useState(false);
+  const navigate = useNavigate();
 
   const localHoneyUser = localStorage.getItem("honey_user");
   const honeyUserObject = JSON.parse(localHoneyUser);
 
   useEffect(() => {
     if (emergency) {
-      const emergencyTickets = tickets.filter((ticket) => ticket.emergency === true);
+      const emergencyTickets = tickets.filter(ticket => ticket.emergency === true);
       setFiltered(emergencyTickets);
+    } else {
+      setFiltered(tickets);
     }
   }, [emergency]);
 
   useEffect(
     () => {
       fetch(`http://localhost:8088/serviceTickets`)
-        .then((response) => response.json())
-        .then((ticketArray) => {
+        .then(response => response.json())
+        .then(ticketArray => {
           setTickets(ticketArray);
         });
     },
@@ -33,26 +38,46 @@ export const TicketList = () => {
       setFiltered(tickets);
     } else {
       // For customers
-      const myTickets = tickets.filter((ticket) => ticket.userId === honeyUserObject.id);
+      const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id);
       setFiltered(myTickets);
     }
   }, [tickets]);
 
   return (
     <>
-      <button
-        onClick={() => {
-          setEmergency(true);
-        }}
-      >
-        Emergency Only
-      </button>
+      {honeyUserObject.staff ? (
+        <>
+          <button
+            onClick={() => {
+              setEmergency(true);
+            }}
+          >
+            Emergency Only
+          </button>
+          <button
+            onClick={() => {
+              setEmergency(false);
+            }}
+          >
+            Show All
+          </button>
+        </>
+      ) : (
+        // New Ticket Form
+        <button
+          onClick={() => {
+            navigate("/ticket/create");
+          }}
+        >
+          Create Ticket
+        </button>
+      )}
 
       <h2>List of Tickets</h2>
       <article className="tickets">
-        {filteredTickets.map((ticket) => {
+        {filteredTickets.map(ticket => {
           return (
-            <section className="ticket" key={`ticket-$ticket.id}`}>
+            <section className="ticket">
               <header>{ticket.description}</header>
               <footer>Emergency: {ticket.emergency ? "🧨" : "No"}</footer>
             </section>
