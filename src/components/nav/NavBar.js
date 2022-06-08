8d3720f50a8808b408c9eaa4ff01b20c406359fe
Rose Cota <rosejcota@gmail.com>
2022-06-07 23:30:31 -0500

    implement EmployeeList view so that any employee can see a list of all employees by clicking the link in the navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -9,12 +9,15 @@ export const NavBar = () => {
             <li className="navbar__item active">
                 <Link className="navbar__link" to="/tickets">Tickets</Link>
             </li>
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/employees">Employees</Link>
+            </li>
             {
                 localStorage.getItem("honey_user")
                     ? <li className="navbar__item navbar__logout">
                         <Link className="navbar__link" to="" onClick={() => {
                             localStorage.removeItem("honey_user")
-                            navigate("/", {replace: true})
+                            navigate("/", { replace: true })
                         }}>Logout</Link>
                     </li>
                     : ""
