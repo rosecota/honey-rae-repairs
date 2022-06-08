9757f7ed9ee94394fad1e4f3620d967096fca2e1
Rose Cota <rosejcota@gmail.com>
2022-06-07 21:49:34 -0500

    add TicketContainer component refactor ticket search functionality implement props

--- src/components/tickets/TicketSearch.js
+++ src/components/tickets/TicketSearch.js
@@ -1,7 +1,13 @@
-export const TicketSearch = () => {
+export const TicketSearch = ({ setterFunction }) => {
   return (
     <div>
-      <input type=
+      <input
+        onChange={
+          (changeEvent) => {
+            setterFunction(changeEvent.target.value)
+          }
+        }
+        type=
         "text" placeholder="Enter search terms" />
     </div>
 
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,7 +1,6 @@
 import { Outlet, Route, Routes } from "react-router-dom";
 import { TicketForm } from "../serviceTickets/TicketForm";
-import { TicketList } from "../tickets/TicketList";
-import { TicketSearch } from "../tickets/TicketSearch";
+import { TicketContainer } from "../tickets/TicketContainer";
 
 export const ApplicationViews = () => {
   return (
@@ -17,12 +16,7 @@ export const ApplicationViews = () => {
           </>
         }
       >
-        <Route path="tickets" element={
-          <>
-            <TicketSearch />
-            <TicketList />
-          </>
-        } />
+        <Route path="tickets" element={<TicketContainer />} />
         <Route path="ticket/create" element={<TicketForm />} />
       </Route>
     </Routes>
